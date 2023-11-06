8bb30de23e23d4e8ead029547e3a47b1957e151f
WidyaPuAs <edithcasean@gmail.com>
2023-11-06 15:21:22 +0700

    inisialisasi project

---
+++ sidebar.php
@@ -0,0 +1,26 @@
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Menu Utama</div>
+                        <a class="nav-link" href="index.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Dashboard
+                        </a>
+                        <a class="nav-link" href="user">
+                            <div class="sb-nav-link-icon"><i class="fas fa-paste"></i></div>
+                            Data User
+                        </a>
+                        <a class="nav-link" href="pelanggan">
+                            <div class="sb-nav-link-icon"><i class="fas fa-paste"></i></div>
+                            Data Pelanggan
+                        </a>
+                        <a class="nav-link" href="kategori">
+                            <div class="sb-nav-link-icon"><i class="fas fa-paste"></i></div>
+                            Data Kategori
+                        </a>
+                        <a class="nav-link" href="menu">
+                            <div class="sb-nav-link-icon"><i class="fas fa-paste"></i></div>
+                            Data Menu
+                        </a>
+                        
+            </nav>
