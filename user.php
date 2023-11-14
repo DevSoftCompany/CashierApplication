06f3494849af9e942326cba71699784ee79753b1
zaskiainayah <zaskiainayah06@gmail.com>
2023-11-14 14:11:10 +0700

    menambahkan user

--- user.php
+++ user.php
@@ -5,34 +5,29 @@ if (!isset($_GET['aksi'])){
                 <h1 class="mt-4">Data User</h1>                      
                 <div class="card mb-4">
                     <div class="card-header">
-                        <a type="button" class="btn btn-primary" href="index.php?page=siswa&aksi=tambah">Tambah Siswa</a>
+                        <a type="button" class="btn btn-primary" href="index.php?page=user&aksi=tambah">Tambah User</a>
                     </div>
                     <div class="card-body">
                         <table id="datatablesSimple">
                             <thead>
                                 <tr>
                                     <th>No</th>
-                                    <th>NIS</th>
-                                    <th>Nama Siswa</th>
-                                    <th>Jenis Kelamin</th>
-                                    <th>Alamat</th>
-                                    <th>Aksi</th>
+                                    <th>Username</th>
+                                    <th>Password</th>
                                 </tr>
                             </thead>
                             <tbody>
                             <?php 
-                            $siswa=mysqli_query($koneksi, "SELECT * FROM siswa");
+                            $user=mysqli_query($koneksi, "SELECT * FROM user");
                             $no = 1;
-                            while ($data = mysqli_fetch_array($siswa)){
+                            while ($data = mysqli_fetch_array($user)){
                             ?>
                                 <tr>
                                     <td><?php echo $no; ?></td>
-                                    <td><?php echo $data['nis']; ?></td>
-                                    <td><?php echo $data['nama_siswa']; ?></td>
-                                    <td><?php echo $data['jenis_kelamin']; ?></td>
-                                    <td><?php echo $data['alamat']; ?></td>
-                                    <td><a href="index.php?page=siswa&aksi=edit&id=<?php echo $data['id_siswa'] ?>">Edit</a> | 
-                                        <a href="index.php?page=siswa&aksi=hapus&id=<?php echo $data['id_siswa'] ?>">Hapus</a></td>
+                                    <td><?php echo $data['username']; ?></td>
+                                    <td><?php echo $data['password']; ?></td>
+                                    <td><a href="index.php?page=user&aksi=edit&id=<?php echo $data['id_user'] ?>">Edit</a> | 
+                                        <a href="index.php?page=user&aksi=hapus&id=<?php echo $data['id_user'] ?>">Hapus</a></td>
                                 </tr>
                             <?php
                             $no++;
@@ -47,32 +42,20 @@ if (!isset($_GET['aksi'])){
 }elseif ($_GET['aksi']=='tambah'){     
 ?>
 <div class="container-fluid px-4">
-                <h1 class="mt-4">Data Siswa</h1>                      
+                <h1 class="mt-4">Data User</h1>                      
                 <div class="card mb-4 col-md-8">
                     <div class="card-header">
-                       <h5> Tambah Siswa </h5>
+                       <h5> Tambah User </h5>
                     </div>
                     <div class="card-body">
                         <form action=''  method="POST" enctype='multipart/form-data'>                        
                                 <div class="form-floating mb-3">
                                     <input class="form-control" type="text" name="a">
-                                    <label>NIS</label>                                
+                                    <label>Nama User</label>                                
                                 </div>                            
                                 <div class="form-floating mb-3">
                                     <input class="form-control" type="text" name="b">
-                                    <label>Nama Siswa</label>
-                                </div>
-                                <div class="form-floating mb-3">
-                                    <input class="form-control" type="text" name="c">
-                                    <label>Jenis Kelamin</label>
-                                </div>
-                                <div class="form-floating mb-3">
-                                    <input class="form-control" type="text" name="d">
-                                    <label>Alamat</label>
-                                </div>
-                                <div class="form-floating mb-3">
-                                    <input class="form-control" type="file" name="e">
-                                    <label>Foto Siswa</label>
+                                    <label>Password</label>
                                 </div>
                                 <div class="d-grid">
                                     <button class="btn btn-primary btn-block" type="submit" name="simpan">Simpan</button>
@@ -84,31 +67,15 @@ if (!isset($_GET['aksi'])){
 
 <?php
 if (isset($_POST['simpan'])){
-    $dir_foto = 'foto/';
-    $filename = basename($_FILES['e']['name']);
-    $uploadfile = $dir_foto . $filename;
-        if ($filename != ''){
-            if (move_uploaded_file($_FILES['e']['tmp_name'], $uploadfile)) {            
-                mysqli_query($koneksi,"INSERT INTO siswa (nis, nama_siswa, jenis_kelamin, alamat, foto_siswa)           
-                                VALUES('$_POST[a]','$_POST[b]','$_POST[c]','$_POST[d]','$filename')");
-                
-                echo "<script>window.alert('Sukses Menambahkan Data Siswa.');
-                        window.location='siswa'</script>";
-            }else{
-                echo "<script>window.alert('Gagal Menambahkan Data Siswa.');
-                        window.location='index.php?page=siswa&aksi=tambah'</script>";
-            }
-        }else{
-                mysqli_query($koneksi,"INSERT INTO siswa (nis, nama_siswa, jenis_kelamin, alamat)           
-                VALUES('$_POST[a]','$_POST[b]','$_POST[c]','$_POST[d]')");
+    mysqli_query($koneksi,"INSERT INTO user (username, password)           
+                VALUES('$_POST[a]','$_POST[b]')");
                                
-                echo "<script>window.alert('Sukses Menambahkan Data Siswa .');
-                        window.location='siswa'</script>";
-        }
+                echo "<script>window.alert('Sukses Menambahkan Data user .');
+                        window.location='user'</script>";
 }
 }elseif ($_GET['aksi']=='edit'){
-    $siswa = mysqli_query($koneksi, "SELECT * FROM siswa where id_siswa='$_GET[id]'");
-    $data = mysqli_fetch_array($siswa);       
+    $user = mysqli_query($koneksi, "SELECT * FROM user where id_user='$_GET[id]'");
+    $data = mysqli_fetch_array($user);       
 ?>
 <div class="container-fluid px-4">
                 <h1 class="mt-4">Data Siswa</h1>                      
@@ -119,24 +86,12 @@ if (isset($_POST['simpan'])){
                     <div class="card-body">
                         <form action=''  method="POST" enctype='multipart/form-data'>      
                             <div class="form-floating mb-3">
-                                <input class="form-control" type="text" name="a" value="<?php echo $data['nis']; ?>">
-                                <label>NIS</label>
+                                <input class="form-control" type="text" name="a" value="<?php echo $data['username']; ?>">
+                                <label>Username</label>
                             </div>
                             <div class="form-floating mb-3">
-                                <input class="form-control" type="text" name="b" value="<?php echo $data['nama_siswa']; ?>">
-                                <label>Nama Siswa</label>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <input class="form-control" type="text" name="c" value="<?php echo $data['jenis_kelamin']; ?>">
-                                <label>Jenis Kelamin</label>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <input class="form-control" type="text" name="d" value="<?php echo $data['alamat']; ?>">
-                                <label>Alamat</label>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <input class="form-control" type="file" name="e">
-                                <label>Foto Siswa</label>
+                                <input class="form-control" type="text" name="b" value="<?php echo $data['password']; ?>">
+                                <label>Password</label>
                             </div>
                             <div class="d-grid">
                                 <button class="btn btn-primary btn-block" type="submit" name="update">Simpan</button>
@@ -152,30 +107,24 @@ if (isset($_POST['update'])){
     $uploadfile = $dir_foto . $filename;
         if ($filename != ''){
             if (move_uploaded_file($_FILES['e']['tmp_name'], $uploadfile)) {            
-                mysqli_query($koneksi,"UPDATE siswa SET nis             = '$_POST[a]',
-                                                        nama_siswa      = '$_POST[b]',
-                                                        jenis_kelamin   = '$_POST[c]',
-                                                        alamat          = '$_POST[d]',
-                                                        foto_siswa      = '$filename' where id_siswa = '$_GET[id]'");           
-                echo "<script>window.alert('Sukses Update Data Siswa.');
-                        window.location='siswa'</script>";
+                mysqli_query($koneksi,"UPDATE user SET username  = '$_POST[a]',
+                                                        password   = '$_POST[b]' where id_user = '$_GET[id]'");           
+                echo "<script>window.alert('Sukses Update Data user.');
+                        window.location='user'</script>";
             }else{
-                echo "<script>window.alert('Gagal Update Data Siswa.');
-                        window.location='index.php?page=siswa&aksi=tambah'</script>";
+                echo "<script>window.alert('Gagal Update Data user.');
+                        window.location='index.php?page=user&aksi=tambah'</script>";
             }
         }else{
-                mysqli_query($koneksi,"UPDATE siswa SET nis             = '$_POST[a]',
-                                                        nama_siswa      = '$_POST[b]',
-                                                        jenis_kelamin   = '$_POST[c]',
-                                                        alamat          = '$_POST[d]',
-                                                        foto_siswa      = '$filename' where id_siswa = '$_GET[id]'");                               
-                echo "<script>window.alert('Sukses Update Data Siswa .');
-                        window.location='siswa'</script>";
+                mysqli_query($koneksi,"UPDATE user SET username    = '$_POST[a]',
+                                                        password    = '$_POST[b]' where id_user = '$_GET[id]'");                               
+                echo "<script>window.alert('Sukses Update Data user .');
+                        window.location='user'</script>";
         }
 }
 }elseif ($_GET['aksi']=='hapus'){ 
-	mysqli_query($koneksi, "DELETE FROM siswa where id_siswa='$_GET[id]'");
-	echo "<script>window.alert('Data Siswa Berhasil Di Hapus.');
-                                window.location='siswa'</script>";
+	mysqli_query($koneksi, "DELETE FROM user where id_user='$_GET[id]'");
+	echo "<script>window.alert('Data user Berhasil Di Hapus.');
+                                window.location='user'</script>";
 }
 ?>
