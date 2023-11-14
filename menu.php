6dab2917dde6ae28299ba446f38fa22382bd340c
unknown <agnescesarsari@gmail.com>
2023-11-14 13:42:02 +0700

    Menambahkan Data Menu

--- menu.php
+++ menu.php
@@ -5,34 +5,35 @@ if (!isset($_GET['aksi'])){
                 <h1 class="mt-4">Data Menu Makanan</h1>                      
                 <div class="card mb-4">
                     <div class="card-header">
-                        <a type="button" class="btn btn-primary" href="index.php?page=siswa&aksi=tambah">Tambah Siswa</a>
+                        <a type="button" class="btn btn-primary" href="index.php?page=menu&aksi=tambah">Tambah Menu</a>
                     </div>
                     <div class="card-body">
                         <table id="datatablesSimple">
                             <thead>
                                 <tr>
                                     <th>No</th>
-                                    <th>NIS</th>
-                                    <th>Nama Siswa</th>
-                                    <th>Jenis Kelamin</th>
-                                    <th>Alamat</th>
-                                    <th>Aksi</th>
+                                    <th>ID Menu</th>
+                                    <th>Nama Menu</th>
+                                    <th>Harga</th>
+                                    <th>Kategori</th>
+                                    <th>Status</th>
                                 </tr>
                             </thead>
                             <tbody>
                             <?php 
-                            $siswa=mysqli_query($koneksi, "SELECT * FROM siswa");
+                            $menu=mysqli_query($koneksi, "SELECT * FROM menu");
                             $no = 1;
-                            while ($data = mysqli_fetch_array($siswa)){
+                            while ($data = mysqli_fetch_array($menu)){
                             ?>
                                 <tr>
                                     <td><?php echo $no; ?></td>
-                                    <td><?php echo $data['nis']; ?></td>
-                                    <td><?php echo $data['nama_siswa']; ?></td>
-                                    <td><?php echo $data['jenis_kelamin']; ?></td>
-                                    <td><?php echo $data['alamat']; ?></td>
-                                    <td><a href="index.php?page=siswa&aksi=edit&id=<?php echo $data['id_siswa'] ?>">Edit</a> | 
-                                        <a href="index.php?page=siswa&aksi=hapus&id=<?php echo $data['id_siswa'] ?>">Hapus</a></td>
+                                    <td><?php echo $data['id_menu']; ?></td>
+                                    <td><?php echo $data['nama_menu']; ?></td>
+                                    <td><?php echo $data['harga']; ?></td>
+                                    <td><?php echo $data['kategori_id']; ?></td>
+                                    <td><?php echo $data['status']; ?></td>
+                                    <td><a href="index.php?page=menu&aksi=edit&id=<?php echo $data['id_menu'] ?>">Edit</a> |
+                                        <a href="index.php?page=menu&aksi=hapus&id=<?php echo $data['id_menu'] ?>">Hapus</a></td>
                                 </tr>
                             <?php
                             $no++;
@@ -47,32 +48,32 @@ if (!isset($_GET['aksi'])){
 }elseif ($_GET['aksi']=='tambah'){     
 ?>
 <div class="container-fluid px-4">
-                <h1 class="mt-4">Data Siswa</h1>                      
+                <h1 class="mt-4">Data Menu Makanan</h1>                      
                 <div class="card mb-4 col-md-8">
                     <div class="card-header">
-                       <h5> Tambah Siswa </h5>
+                       <h5> Tambah Menu </h5>
                     </div>
                     <div class="card-body">
                         <form action=''  method="POST" enctype='multipart/form-data'>                        
                                 <div class="form-floating mb-3">
                                     <input class="form-control" type="text" name="a">
-                                    <label>NIS</label>                                
+                                    <label>ID Menu</label>                                
                                 </div>                            
                                 <div class="form-floating mb-3">
                                     <input class="form-control" type="text" name="b">
-                                    <label>Nama Siswa</label>
+                                    <label>Nama Menu</label>
                                 </div>
                                 <div class="form-floating mb-3">
                                     <input class="form-control" type="text" name="c">
-                                    <label>Jenis Kelamin</label>
+                                    <label>Harga</label>
                                 </div>
                                 <div class="form-floating mb-3">
                                     <input class="form-control" type="text" name="d">
-                                    <label>Alamat</label>
+                                    <label>Kategori</label>
                                 </div>
                                 <div class="form-floating mb-3">
-                                    <input class="form-control" type="file" name="e">
-                                    <label>Foto Siswa</label>
+                                    <input class="form-control" type="text" name="e">
+                                    <label>Status</label>
                                 </div>
                                 <div class="d-grid">
                                     <button class="btn btn-primary btn-block" type="submit" name="simpan">Simpan</button>
@@ -83,60 +84,44 @@ if (!isset($_GET['aksi'])){
 </div>
 
 <?php
-if (isset($_POST['simpan'])){
-    $dir_foto = 'foto/';
-    $filename = basename($_FILES['e']['name']);
-    $uploadfile = $dir_foto . $filename;
-        if ($filename != ''){
-            if (move_uploaded_file($_FILES['e']['tmp_name'], $uploadfile)) {            
-                mysqli_query($koneksi,"INSERT INTO siswa (nis, nama_siswa, jenis_kelamin, alamat, foto_siswa)           
-                                VALUES('$_POST[a]','$_POST[b]','$_POST[c]','$_POST[d]','$filename')");
-                
-                echo "<script>window.alert('Sukses Menambahkan Data Siswa.');
-                        window.location='siswa'</script>";
-            }else{
-                echo "<script>window.alert('Gagal Menambahkan Data Siswa.');
-                        window.location='index.php?page=siswa&aksi=tambah'</script>";
-            }
-        }else{
-                mysqli_query($koneksi,"INSERT INTO siswa (nis, nama_siswa, jenis_kelamin, alamat)           
-                VALUES('$_POST[a]','$_POST[b]','$_POST[c]','$_POST[d]')");
-                               
-                echo "<script>window.alert('Sukses Menambahkan Data Siswa .');
-                        window.location='siswa'</script>";
-        }
+if (isset($_POST['simpan'])) {
+    mysqli_query($koneksi, "INSERT INTO menu (id_menu, nama_menu, harga, kategori_id, status)           
+                            VALUES('$_POST[a]','$_POST[b]','$_POST[c]','$_POST[d]','$_POST[e]')");
+
+    echo "<script>window.alert('Sukses Menambahkan Data Menu Makanan');
+                    window.location='menu'</script>";
 }
 }elseif ($_GET['aksi']=='edit'){
-    $siswa = mysqli_query($koneksi, "SELECT * FROM siswa where id_siswa='$_GET[id]'");
-    $data = mysqli_fetch_array($siswa);       
+    $menu = mysqli_query($koneksi, "SELECT * FROM menu where id_menu='$_GET[id]'");
+    $data = mysqli_fetch_array($menu);       
 ?>
 <div class="container-fluid px-4">
-                <h1 class="mt-4">Data Siswa</h1>                      
+                <h1 class="mt-4">Data Menu Makanan</h1>                      
                 <div class="card mb-4 col-md-8">
                     <div class="card-header">
-                       <h5> Update Siswa </h5>
+                       <h5> Update Menu </h5>
                     </div>
                     <div class="card-body">
                         <form action=''  method="POST" enctype='multipart/form-data'>      
                             <div class="form-floating mb-3">
-                                <input class="form-control" type="text" name="a" value="<?php echo $data['nis']; ?>">
-                                <label>NIS</label>
+                                <input class="form-control" type="text" name="a" value="<?php echo $data['id_menu']; ?>">
+                                <label>ID Menu</label>
                             </div>
                             <div class="form-floating mb-3">
-                                <input class="form-control" type="text" name="b" value="<?php echo $data['nama_siswa']; ?>">
-                                <label>Nama Siswa</label>
+                                <input class="form-control" type="text" name="b" value="<?php echo $data['nama_menu']; ?>">
+                                <label>Nama Menu</label>
                             </div>
                             <div class="form-floating mb-3">
-                                <input class="form-control" type="text" name="c" value="<?php echo $data['jenis_kelamin']; ?>">
-                                <label>Jenis Kelamin</label>
+                                <input class="form-control" type="text" name="c" value="<?php echo $data['harga']; ?>">
+                                <label>Harga</label>
                             </div>
                             <div class="form-floating mb-3">
-                                <input class="form-control" type="text" name="d" value="<?php echo $data['alamat']; ?>">
-                                <label>Alamat</label>
+                                <input class="form-control" type="text" name="d" value="<?php echo $data['kategori_id']; ?>">
+                                <label>Kategori</label>
                             </div>
                             <div class="form-floating mb-3">
-                                <input class="form-control" type="file" name="e">
-                                <label>Foto Siswa</label>
+                                <input class="form-control" type="text" name="e" value="<?php echo $data['status']; ?>">
+                                <label>Status</label>
                             </div>
                             <div class="d-grid">
                                 <button class="btn btn-primary btn-block" type="submit" name="update">Simpan</button>
@@ -146,36 +131,18 @@ if (isset($_POST['simpan'])){
                 </div>
 </div>
 <?php
-if (isset($_POST['update'])){
-    $dir_foto = 'foto/';
-    $filename = basename($_FILES['e']['name']);
-    $uploadfile = $dir_foto . $filename;
-        if ($filename != ''){
-            if (move_uploaded_file($_FILES['e']['tmp_name'], $uploadfile)) {            
-                mysqli_query($koneksi,"UPDATE siswa SET nis             = '$_POST[a]',
-                                                        nama_siswa      = '$_POST[b]',
-                                                        jenis_kelamin   = '$_POST[c]',
-                                                        alamat          = '$_POST[d]',
-                                                        foto_siswa      = '$filename' where id_siswa = '$_GET[id]'");           
-                echo "<script>window.alert('Sukses Update Data Siswa.');
-                        window.location='siswa'</script>";
-            }else{
-                echo "<script>window.alert('Gagal Update Data Siswa.');
-                        window.location='index.php?page=siswa&aksi=tambah'</script>";
-            }
-        }else{
-                mysqli_query($koneksi,"UPDATE siswa SET nis             = '$_POST[a]',
-                                                        nama_siswa      = '$_POST[b]',
-                                                        jenis_kelamin   = '$_POST[c]',
-                                                        alamat          = '$_POST[d]',
-                                                        foto_siswa      = '$filename' where id_siswa = '$_GET[id]'");                               
-                echo "<script>window.alert('Sukses Update Data Siswa .');
-                        window.location='siswa'</script>";
-        }
+ if (isset($_POST['update'])) {
+    mysqli_query($koneksi, "UPDATE menu SET id_menu = '$_POST[a]',
+                                            nama_menu = '$_POST[b]', 
+                                            harga = '$_POST[c]', 
+                                            kategori_id = '$_POST[d]', 
+                                            status = '$_POST[e]' where id_kategori = '$_GET[id]'");
+    echo "<script>window.alert('Sukses Update Data Menu.');
+                    window.location='menu'</script>";
 }
-}elseif ($_GET['aksi']=='hapus'){ 
-	mysqli_query($koneksi, "DELETE FROM siswa where id_siswa='$_GET[id]'");
-	echo "<script>window.alert('Data Siswa Berhasil Di Hapus.');
-                                window.location='siswa'</script>";
+} elseif ($_GET['aksi'] == 'hapus') {
+mysqli_query($koneksi, "DELETE FROM menu where id_menu='$_GET[id]'");
+echo "<script>window.alert('Data Menu Berhasil Di Hapus.');
+                            window.location='menu'</script>";
 }
 ?>
